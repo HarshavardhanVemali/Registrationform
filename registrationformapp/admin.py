7d9c591e3caf5971378472b57efc8194888c9bcd
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-08-01 17:30:29 +0530

    changescommit

--- registrationformapp/admin.py
+++ registrationformapp/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Register
 
 class AdminRegister(admin.ModelAdmin):
-    list_display =('name','email','branch','year','concept_to_present','slot_number')
-    list_filter =('name','email','branch','year','concept_to_present','slot_number')
-    search_fields = ('name','email','branch','year','concept_to_present','slot_number')
+    list_display =('register_number','name','email','branch','year','concept_to_present','slot_number')
+    list_filter =('register_number','name','email','branch','year','concept_to_present','slot_number')
+    search_fields = ('register_number','name','email','branch','year','concept_to_present','slot_number')
 
 admin.site.register(Register,AdminRegister)
