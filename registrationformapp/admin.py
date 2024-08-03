d5a07180576fd62bf2865f436454add92280c96d
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-08-03 13:22:30 +0530

    changescommit

--- registrationformapp/admin.py
+++ registrationformapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Register
 
 class AdminRegister(admin.ModelAdmin):
-    list_display =('register_number','name','email','branch','year','concept_to_present','slot_number')
+    list_display =('register_number','name','email','branch','year','concept_to_present','slot_number','evalutor_1','evalutor_2','average')
     list_filter =('register_number','name','email','branch','year','concept_to_present','slot_number')
     search_fields = ('register_number','name','email','branch','year','concept_to_present','slot_number')
 
--- registrationformapp/models.py
+++ registrationformapp/models.py
@@ -7,20 +7,21 @@ class Register(models.Model):
     branch = models.CharField(max_length=10, null=True)
     year = models.CharField(max_length=10, null=True)
     concept_to_present = models.CharField(max_length=30, null=True)
-    slot_number = models.PositiveIntegerField(null=True, blank=True) 
-    evalutor_1=models.IntegerField(null=True)
-    evalutor_2=models.IntegerField(null=True)
+    slot_number = models.PositiveIntegerField(null=True, blank=True)
+    evalutor_1 = models.IntegerField(null=True)
+    evalutor_2 = models.IntegerField(null=True)
+    average = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.register_number}"
 
     def save(self, *args, **kwargs):
-        if not self.slot_number:  # Assign slot number only if it's not already assigned
-            # Get the highest existing slot number
+        if not self.slot_number:
             last_slot = Register.objects.all().order_by('-slot_number').first()
             last_slot_number = last_slot.slot_number if last_slot else 0
-
-            # Assign the next available slot number
             self.slot_number = last_slot_number + 1
+
+        if self.evalutor_1 is not None and self.evalutor_2 is not None:
+            self.average = (self.evalutor_1 + self.evalutor_2) / 2
 
         super().save(*args, **kwargs)
