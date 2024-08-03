ce03571503b84932d0143d7445cd8f090ffa5205
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-08-03 13:40:53 +0530

    changecommit

--- registrationformapp/admin.py
+++ registrationformapp/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Register
 
-class AdminRegister(admin.ModelAdmin):
-    list_display =('register_number','name','email','branch','year','concept_to_present','slot_number','evalutor_1','evalutor_2','average')
-    list_filter =('register_number','name','email','branch','year','concept_to_present','slot_number')
-    search_fields = ('register_number','name','email','branch','year','concept_to_present','slot_number')
+class AverageUpdatedFilter(admin.SimpleListFilter):
+    title = 'Average Updated'
+    parameter_name = 'average_updated'
 
-admin.site.register(Register,AdminRegister)
+    def lookups(self, request, model_admin):
+        return (
+            ('updated', 'Updated'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'updated':
+            return queryset.exclude(average__isnull=True)
+        return queryset
+
+class AdminRegister(admin.ModelAdmin):
+    list_display = ('register_number', 'name', 'email', 'branch', 'year', 'concept_to_present', 'slot_number', 'evalutor_1', 'evalutor_2', 'average')
+    list_filter = ('register_number', 'name', 'email', 'branch', 'year', 'concept_to_present', 'slot_number', 'average', AverageUpdatedFilter)
+    search_fields = ('register_number', 'name', 'email', 'branch', 'year', 'concept_to_present', 'slot_number')
+
+admin.site.register(Register, AdminRegister)
+
